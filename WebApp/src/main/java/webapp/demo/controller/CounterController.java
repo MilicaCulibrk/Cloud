1776b78a50ecba3578f5015c5b6356fa74ca1eb9
MilicaCulibrk <milica6.1.97thebest@gmail.com>
2020-12-01 14:16:51 +0100

    added environment variables

--- WebApp/src/main/java/webapp/demo/controller/CounterController.java
+++ WebApp/src/main/java/webapp/demo/controller/CounterController.java
@@ -8,20 +8,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import webapp.demo.service.CounterService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @CrossOrigin
-@RequestMapping("/counter")
+@RequestMapping("")
 @Controller
 public class CounterController {
 
     @Autowired
     CounterService counterService;
 
-    @GetMapping(path = "", produces = "application/json")
+    @GetMapping(path = "/counter", produces = "application/json")
     public ResponseEntity getCounter() {
 
         int counter = counterService.getCounter();
 
         return new ResponseEntity<>(counter, HttpStatus.OK) ;
+    }
+
+    @GetMapping(path = "/environment_variables", produces = "application/json")
+    public ResponseEntity getEnvironmentVariables() {
+
+        List<Object> environmentVariables = new ArrayList<>();
+        environmentVariables.add(System.getenv("DB_URL"));
+        environmentVariables.add(System.getenv("DB_USERNAME"));
+        environmentVariables.add(System.getenv("DB_PASSWORD"));
+
+        return new ResponseEntity<>(environmentVariables, HttpStatus.OK) ;
     }
 
 }
